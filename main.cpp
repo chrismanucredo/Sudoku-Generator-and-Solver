06100d329eaff75c94f69d2a83294f5aacd942d0
Christoph Manucredo <chris.manucredo@gmail.com>
2020-05-21 15:01:32 +0200

    added timing of the generation algorithm

--- main.cpp
+++ main.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
 #include "Board.h"
+#include <chrono>
 
 int main() {
 
     //initializing a board
     Board newBoard;
 
+    //start time of generation
+    auto start = std::chrono::high_resolution_clock::now();
+
     //creating a coherent sudoku board
     newBoard.generateBoard();
 
+    //stop time of generation
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = duration_cast<std::chrono::microseconds>(stop - start);
+
     //printing the board
     newBoard.printBoard();
+
+    //printing generation duration
+    std::cout << std::endl;
+    std::cout << "Generating the sudoku board took: " << duration.count() << " ms" << std::endl;
 
 
     return 0;
